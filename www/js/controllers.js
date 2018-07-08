475ca05e775553e71ca4e30b0838225567b81ef5
jessamynsmith <jessamyn.smith@gmail.com>
2018-07-08 13:57:56 -0600

    Added angular maphilight.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -50,4 +50,28 @@ angular.module('starter.controllers', [])
 
 .controller('MapCtrl', function($scope, $stateParams) {
   $scope.mapId = $stateParams.mapId;
+
+  $scope.config = {
+
+		fade: true,
+		alwaysOn: true,
+		neverOn: false,
+
+		// fill
+		fill: true,
+		fillColor: '#ffffff',
+		fillOpacity: 0.4,
+
+		// stroke
+		stroke: true,
+		strokeColor: '#4d0ec0',
+		strokeOpacity: 1,
+		strokeWidth: 1,
+
+    // shadow:
+    shadow: true,
+    shadowColor: '#000000',
+    shadowOpacity: 0.8,
+    shadowRadius: 10
+  };
 });
